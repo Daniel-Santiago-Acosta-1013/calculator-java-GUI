82f50622c687247a05d3b7a8dea5396652f64190
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-09-01 12:24:08 -0500

    [add] mejorando diseño

--- src/main/java/org/example/CalculatorApp.java
+++ src/main/java/org/example/CalculatorApp.java
@@ -1,11 +1,15 @@
+
 package org.example;
 
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import org.mariuszgromada.math.mxparser.Argument;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.effect.DropShadow;
 import org.mariuszgromada.math.mxparser.Expression;
 import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
 import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
@@ -36,52 +40,72 @@ public class CalculatorApp {
     private VBox createCalculatorPane() {
         VBox vBox = new VBox(10);
         GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
 
         String[][] buttons = {
                 {"7", "8", "9", "/"},
                 {"4", "5", "6", "*"},
                 {"1", "2", "3", "-"},
-                {"0", ".", "(", "+"}
+                {".", "0", "(", "+"}
         };
+
+        DropShadow shadow = new DropShadow();
+        shadow.setColor(Color.BLUE);
+        shadow.setRadius(5.0);
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 Button btn = new Button(buttons[i][j]);
-                btn.setPrefSize(50, 50);
+                btn.setPrefSize(60, 60);
+                btn.setStyle("-fx-background-color: #f0f0f0; -fx-font-size: 18px;");
+                btn.setOnMousePressed(e -> btn.setEffect(shadow));
+                btn.setOnMouseReleased(e -> btn.setEffect(null));
                 btn.setOnAction(e -> buttonPressed(btn.getText()));
-                grid.add(btn, j, i + 1);
+                grid.add(btn, j, i);
             }
         }
 
+        // Equals, Backspace and Clear button layout adjustments
+        HBox controlButtons = new HBox(10);
+        controlButtons.setAlignment(Pos.CENTER);
+
         // Equals button
         Button equalsBtn = new Button("=");
-        equalsBtn.setPrefSize(50, 50);
+        equalsBtn.setPrefSize(60, 60);
+        equalsBtn.setStyle("-fx-background-color: #4CAF50; -fx-font-size: 18px; -fx-text-fill: white;");
+        equalsBtn.setOnMousePressed(e -> equalsBtn.setEffect(shadow));
+        equalsBtn.setOnMouseReleased(e -> equalsBtn.setEffect(null));
         equalsBtn.setOnAction(e -> evaluateExpression());
-        grid.add(equalsBtn, 3, 5);
 
         // Backspace button
         Button backspaceBtn = new Button("Borrar");
-        backspaceBtn.setPrefSize(100, 50);
+        backspaceBtn.setPrefSize(90, 60);
+        backspaceBtn.setStyle("-fx-font-size: 18px;");
         backspaceBtn.setOnAction(e -> {
             String currentText = display.getText();
             if (!currentText.isEmpty()) {
                 display.setText(currentText.substring(0, currentText.length() - 1));
             }
         });
-        grid.add(backspaceBtn, 1, 5, 2, 1); // Span 2 columns
 
         // Clear button
         Button clearBtn = new Button("C");
-        clearBtn.setPrefSize(50, 50);
+        clearBtn.setPrefSize(60, 60);
+        clearBtn.setStyle("-fx-background-color: #f44336; -fx-font-size: 18px; -fx-text-fill: white;");
         clearBtn.setOnAction(e -> display.clear());
-        grid.add(clearBtn, 0, 5);
 
-        display.setPrefWidth(200);
-        grid.add(display, 0, 0, 4, 1);
+        controlButtons.getChildren().addAll(backspaceBtn, clearBtn, equalsBtn);
+        display.setPrefWidth(300);
+        display.setStyle("-fx-font-size: 20px;");
 
-        vBox.getChildren().addAll(display, grid);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.getChildren().addAll(display, grid, controlButtons);
         return vBox;
     }
+
+    // !hasta aqui modifico el codigo ----------------------------------------------------------
 
     private ListView<String> createHistoryPane() {
         return historyView;
